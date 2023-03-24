5e72062d0c045040325473dcdc9ec3bdc1c0c9af
Physic_Hybrid <85246569+PhysicHybridTG@users.noreply.github.com>
2023-03-24 19:40:55 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,9 +42,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton(' Search', switch_inline_query_current_chat=''),
+           
             InlineKeyboardButton(' Updates', url='https://t.me/+Bd5SeJPRstFjNjc1')
             ],[
             InlineKeyboardButton('Help', callback_data='help'),
@@ -88,9 +86,7 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton(' Search', switch_inline_query_current_chat=''),
+          
             InlineKeyboardButton(' Updates', url='https://t.me/+Bd5SeJPRstFjNjc1')
             ],[
             InlineKeyboardButton(' Help', callback_data='help'),
